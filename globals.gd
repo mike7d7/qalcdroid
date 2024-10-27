09ee1d5fae2a00f87640acdfd04eb2cad3b833ee
mike7d7 <mike7d7@proton.me>
2024-10-27 12:09:38 -0600

    delete old calcSwipe function (unneeded after switch to bettertabcontainer)

--- globals.gd
+++ globals.gd
@@ -22,22 +22,3 @@ func _ready() -> void:
 	interval_switch.set_pressed_no_signal(user_prefs.interval)
 	fractions_group.get_buttons()[user_prefs.fraction].set_pressed_no_signal(true)
 	angle_unit_group.get_buttons()[user_prefs.angle_unit].set_pressed_no_signal(true)
-
-
-func _input(event):
-	pass
-	#if event is InputEventScreenTouch && event.position.y > result_string.global_position.y + result_string.size.y:
-		#if event.pressed:
-			#swipe_start = event.position
-		#else:
-			#calcSwipe(event.position)
-
-func calcSwipe(swipe_end):
-	if swipe_start == null:
-		return
-	var swipe = swipe_end - swipe_start
-	if abs(swipe.x) > 250:
-		if swipe.x < 0:
-			get_tree().current_scene.get_node("%TabContainer").select_next_available()
-		else:
-			get_tree().current_scene.get_node("%TabContainer").select_previous_available()
