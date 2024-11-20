9adaa1bf0effa5cd4cb82b8ab3ab888f6faa911e
mike7d7 <mike7d7@proton.me>
2024-11-20 09:37:33 -0600

    make clear button also clear result

--- button_scripts/clear.gd
+++ button_scripts/clear.gd
@@ -1,6 +1,7 @@
 extends Button
 
 @onready var input_node = get_tree().current_scene.get_node("%input")
+@onready var label = get_tree().current_scene.get_node("%Label")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.pressed.connect(self._button_pressed)
@@ -8,4 +9,5 @@ func _ready() -> void:
 
 func _button_pressed():
 	input_node.text = ''
+	label.text = '0'
 	input_node.grab_focus()
