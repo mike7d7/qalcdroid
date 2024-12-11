c2049b0dfa3deaead4aae7b55d32d0e67c77afbf
mike7d7 <mike7d7@proton.me>
2024-12-11 14:33:45 -0600

    add statically typed vars to input.gd

--- input.gd
+++ input.gd
@@ -1,39 +1,39 @@
 extends CodeEdit
-@onready var cpp_code = $"../../VBoxContainer/TabContainer/numbers/GDExample"
-@onready var popup = $"../../fn_popup/ScrollContainer/VBoxContainer/GridContainer"
-@onready var tree_node = $"../../VBoxContainer/TabContainer/Units/Tree"
+@onready var cpp_code: Node = $"../../VBoxContainer/TabContainer/numbers/GDExample"
+@onready var popup: Node = $"../../fn_popup/ScrollContainer/VBoxContainer/GridContainer"
+@onready var tree_node: Node = $"../../VBoxContainer/TabContainer/Units/Tree"
 
-func _ready():
+func _ready() -> void:
 	grab_focus()
 	
 
 #code for units
-func _on_tree_item_activated():
-	var item = tree_node.get_selected()
+func _on_tree_item_activated() -> void:
+	var item: TreeItem = tree_node.get_selected()
 	if item.get_metadata(0):
 		self.insert_text_at_caret(item.get_metadata(0), -1)
 	else:
 		item.set_collapsed_recursive(!item.collapsed)
 
 #code for functions
-func _on_functions_item_activated(item, title, treeitem) -> void:
+func _on_functions_item_activated(item, title: String, treeitem: TreeItem) -> void: # 'item' can be null or string
 	while popup.get_child_count() > 1:
 		popup.get_child(0).free()
 	if item:
-		var max_args = cpp_code.get_max_args(item)
+		var max_args: int = cpp_code.get_max_args(item)
 		if max_args == -1:
 			max_args = cpp_code.get_min_args(item)
 		if max_args == 0:
 			max_args = 1
 		for i in range(max_args):
-			var label = Label.new()
+			var label: Node = Label.new()
 			popup.add_child(label)
 			label.set_text(cpp_code.get_arg_name(item, i + 1))
 			
-			var default_value = cpp_code.get_arg_def_val(item, i + 1)
+			var default_value: String = cpp_code.get_arg_def_val(item, i + 1)
 			
-			var input_type = cpp_code.get_arg_type(item, i + 1)
-			var input
+			var input_type: int = cpp_code.get_arg_type(item, i + 1)
+			var input: Node
 			match input_type:
 				0:
 					input = LineEdit.new()
@@ -53,7 +53,7 @@ func _on_functions_item_activated(item, title, treeitem) -> void:
 					input = OptionButton.new()
 					#TODO add working combobox
 			popup.add_child(input)
-			var separator = HSeparator.new()
+			var separator: Node = HSeparator.new()
 			popup.add_child(separator)
 			
 		#popup.move_child(popup.get_child(1), popup.get_child_count() - 1)
@@ -66,9 +66,9 @@ func _on_functions_item_activated(item, title, treeitem) -> void:
 		treeitem.set_collapsed_recursive(!treeitem.collapsed)
 
 #code for variables
-@onready var variable_tree_node = $"../../VBoxContainer/TabContainer/Variables/VariableTree"
+@onready var variable_tree_node: Node = $"../../VBoxContainer/TabContainer/Variables/VariableTree"
 func _on_variable_tree_item_activated() -> void:
-	var item = variable_tree_node.get_selected()
+	var item: TreeItem = variable_tree_node.get_selected()
 	if item.get_metadata(0):
 		self.insert_text_at_caret(item.get_metadata(0), -1)
 	else:
