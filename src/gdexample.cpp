d2b8b59f28fd8152c74de079b318b71a3254b6cf
mike7d7 <mike7d7@proton.me>
2025-01-02 17:32:33 -0600

    remove unused fetch_exchange_rates() and add reload_exchange_rates()

--- src/gdexample.cpp
+++ src/gdexample.cpp
@@ -1,5 +1,7 @@
 #include "gdexample.h"
 #include "libqalculate/includes.h"
+#include <godot_cpp/classes/dir_access.hpp>
+#include <godot_cpp/classes/file_access.hpp>
 #include <godot_cpp/variant/utility_functions.hpp>
 #include <godot_cpp/variant/variant.hpp>
 
@@ -13,6 +15,12 @@ using namespace godot;
 /*UtilityFunctions::print();*/
 
 GDExample::GDExample() {
+#ifdef __ANDROID_API__
+  if (!FileAccess::file_exists("user://eurofxref-daily.xml")) {
+    Ref<godot::DirAccess> dir = DirAccess::open("res://");
+    dir->copy("res://eurofxref-daily.xml", "user://eurofxref-daily.xml");
+  }
+#endif
   eo = default_user_evaluation_options;
   po = default_print_options;
   po.interval_display = INTERVAL_DISPLAY_SIGNIFICANT_DIGITS;
@@ -167,14 +175,8 @@ void GDExample::_change_angle_unit(int angle_unit) {
   }
 }
 
-bool GDExample::_fetch_exchange_rates() {
-    if (calc->canFetch()) {
-        UtilityFunctions::print("can fetch");
-        return calc->fetchExchangeRates();
-    } else {
-        UtilityFunctions::print("cannot fetch");
-        return false;
-    }
+bool GDExample::_reload_exchange_rates() {
+  return calc->loadExchangeRates() && calc->loadGlobalCurrencies();
 }
 
 void GDExample::_bind_methods() {
@@ -200,7 +202,7 @@ void GDExample::_bind_methods() {
                        &GDExample::_change_fraction);
   ClassDB::bind_method(D_METHOD("_change_angle_unit"),
                        &GDExample::_change_angle_unit);
-  ClassDB::bind_method(D_METHOD("_fetch_exchange_rates"),
-                       &GDExample::_fetch_exchange_rates);
+  ClassDB::bind_method(D_METHOD("_reload_exchange_rates"),
+                       &GDExample::_reload_exchange_rates);
   /*ADD_SIGNAL(MethodInfo("signal1"))*/
 }
