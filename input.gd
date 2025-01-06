bfb18dfbb32dc1b8aab41c7e7a9a3e928ca4a28d
mike7d7 <mike7d7@proton.me>
2025-01-06 17:35:42 -0600

    fill functions tree using threads

--- input.gd
+++ input.gd
@@ -17,23 +17,27 @@ func _on_tree_item_activated() -> void:
 		item.set_collapsed_recursive(!item.collapsed)
 
 #code for functions
-func _on_functions_item_activated(item, title: String, treeitem: TreeItem) -> void: # 'item' can be null or string
+@onready var functions_tree_node: Node = $"../../VBoxContainer/TabContainer/Functions"
+func _on_functions_item_activated() -> void:
+	var treeitem: TreeItem = functions_tree_node.get_child(-1).get_selected()
+	var item_string = treeitem.get_metadata(0) # 'item' can be null or string
+	var title: String = treeitem.get_text(0)
 	while popup.get_child_count() > 1:
 		popup.get_child(0).free()
-	if item:
-		var max_args: int = cpp_code.get_max_args(item)
+	if item_string:
+		var max_args: int = cpp_code.get_max_args(item_string)
 		if max_args == -1:
-			max_args = cpp_code.get_min_args(item)
+			max_args = cpp_code.get_min_args(item_string)
 		if max_args == 0:
 			max_args = 1
 		for i in range(max_args):
 			var label: Node = Label.new()
 			popup.add_child(label)
-			label.set_text(cpp_code.get_arg_name(item, i + 1))
+			label.set_text(cpp_code.get_arg_name(item_string, i + 1))
 			
-			var default_value: String = cpp_code.get_arg_def_val(item, i + 1)
+			var default_value: String = cpp_code.get_arg_def_val(item_string, i + 1)
 			
-			var input_type: int = cpp_code.get_arg_type(item, i + 1)
+			var input_type: int = cpp_code.get_arg_type(item_string, i + 1)
 			var input: Node
 			match input_type:
 				0:
@@ -60,7 +64,7 @@ func _on_functions_item_activated(item, title: String, treeitem: TreeItem) -> vo
 		#popup.move_child(popup.get_child(1), popup.get_child_count() - 1)
 		popup.move_child(popup.get_child(0), popup.get_child_count() - 1)
 		$"../../fn_popup/ScrollContainer/VBoxContainer/Label".set_text(title)
-		$"../../fn_popup/ScrollContainer/VBoxContainer/Label".set_meta("metadata", item)
+		$"../../fn_popup/ScrollContainer/VBoxContainer/Label".set_meta("metadata", item_string)
 		$"../../fn_popup".size = $"../../../Control".size
 		$"../../fn_popup".show()
 	else:
