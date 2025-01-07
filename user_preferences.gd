888682890b6f63ab30b7e4d8eb023bfc4ffca8e6
mike7d7 <mike7d7@proton.me>
2025-01-06 22:58:23 -0600

    use binary format for saving user preferences

--- user_preferences.gd
+++ user_preferences.gd
@@ -8,10 +8,10 @@ class_name UserPreferences extends Resource
 @export var tab_swipe: bool = true
 
 func save() -> void:
-	ResourceSaver.save(self, "user://user_prefs.tres")
+	ResourceSaver.save(self, "user://user_prefs.res")
 
 static func load_or_create() -> UserPreferences:
-	var res: UserPreferences = load("user://user_prefs.tres") as UserPreferences
+	var res: UserPreferences = load("user://user_prefs.res") as UserPreferences
 	if !res:
 		res = UserPreferences.new()
 		
