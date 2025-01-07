362334b5a2f3a3151ffa041c6c5805b0e92e643c
mike7d7 <mike7d7@proton.me>
2025-01-06 23:10:26 -0600

    fix function button on numbers2 not working

--- button_scripts/insert_function_shortcut.gd
+++ button_scripts/insert_function_shortcut.gd
@@ -6,4 +6,4 @@ func _ready() -> void:
 	self.pressed.connect(self._button_pressed.bind())
 
 func _button_pressed() -> void:
-	get_tree().current_scene.get_node("%input")._on_functions_item_activated(self.get_meta("item"), self.get_meta("title"), null)
+	get_tree().current_scene.get_node("%input")._on_functions_item_activated_grid(self.get_meta("item"), self.get_meta("title"), null)
--- input.gd
+++ input.gd
@@ -70,6 +70,56 @@ func _on_functions_item_activated() -> void:
 	else:
 		treeitem.set_collapsed_recursive(!treeitem.collapsed)
 
+#code for functions when calling from buttons in numbers2
+func _on_functions_item_activated_grid(item, title: String, treeitem: TreeItem) -> void: # 'item' can be null or string
+	while popup.get_child_count() > 1:
+		popup.get_child(0).free()
+	if item:
+		var max_args: int = cpp_code.get_max_args(item)
+		if max_args == -1:
+			max_args = cpp_code.get_min_args(item)
+		if max_args == 0:
+			max_args = 1
+		for i in range(max_args):
+			var label: Node = Label.new()
+			popup.add_child(label)
+			label.set_text(cpp_code.get_arg_name(item, i + 1))
+			
+			var default_value: String = cpp_code.get_arg_def_val(item, i + 1)
+			
+			var input_type: int = cpp_code.get_arg_type(item, i + 1)
+			var input: Node
+			match input_type:
+				0:
+					input = LineEdit.new()
+					if default_value == "undefined":
+						input.set_placeholder("optional")
+					elif default_value:
+						input.set_text(default_value)
+				1:
+					input = CheckButton.new()
+				2:
+					input = SpinBox.new()
+					input.set_allow_lesser(true)
+					input.set_allow_greater(true)
+					if default_value:
+						input.set_value_no_signal(default_value.to_float())
+				3:
+					input = OptionButton.new()
+					#TODO add working combobox
+			popup.add_child(input)
+			var separator: Node = HSeparator.new()
+			popup.add_child(separator)
+			
+		#popup.move_child(popup.get_child(1), popup.get_child_count() - 1)
+		popup.move_child(popup.get_child(0), popup.get_child_count() - 1)
+		$"../../fn_popup/ScrollContainer/VBoxContainer/Label".set_text(title)
+		$"../../fn_popup/ScrollContainer/VBoxContainer/Label".set_meta("metadata", item)
+		$"../../fn_popup".size = $"../../../Control".size
+		$"../../fn_popup".show()
+	else:
+		treeitem.set_collapsed_recursive(!treeitem.collapsed)
+
 #code for variables
 @onready var variable_tree_node: Node = $"../../VBoxContainer/TabContainer/Variables"
 func _on_variable_tree_item_activated() -> void:
