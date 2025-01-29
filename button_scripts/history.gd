4cd16cc4aa9c9796373fc1d1a91d0d82adb6403c
mike7d7 <mike7d7@proton.me>
2025-01-28 18:48:03 -0600

    when pressing a history entry, write it to input and close the popup

--- button_scripts/history.gd
+++ button_scripts/history.gd
@@ -15,10 +15,12 @@ func fill_history_container() -> void:
 		var entry: Button = Button.new()
 		entry.text = i
 		entry.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
+		entry.set_script(preload("res://button_scripts/history_entry_button.gd"))
 		get_node("/root/Control/history_popup/VBoxContainer/ScrollContainer/VBoxContainer").add_child(entry)
 		
 func add_entry_to_history() -> void:
 	var entry: Button = Button.new()
 	entry.text = Globals.user_prefs.history[-1]
 	entry.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
+	entry.set_script(preload("res://button_scripts/history_entry_button.gd"))
 	get_node("/root/Control/history_popup/VBoxContainer/ScrollContainer/VBoxContainer").add_child(entry)
