ab4783ea7d4d67067f3fa1275280d33409979160
mike7d7 <mike7d7@proton.me>
2025-03-05 19:50:10 -0600

    up and down buttons: move caret at end of text when changing input_node.text

--- button_scripts/move_down.gd
+++ button_scripts/move_down.gd
@@ -10,6 +10,9 @@ func _button_pressed() -> void:
 	if Globals.entry_number < -1:
 		Globals.entry_number += 1
 		input_node.text = Globals.history[Globals.entry_number]
+		input_node.set_caret_column(input_node.text.length())
+		input_node.scroll_vertical = input_node.get_line_wrap_count(0) - 1
+		input_node.grab_focus()
 	elif Globals.entry_number == -1:
 		Globals.entry_number += 1
 		input_node.text = ""
